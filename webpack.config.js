55256b8a1d644a664af1406a7e8c7ab3ca956a19
ardydj <ardydj07@gmail.com>
2020-09-25 15:46:06 -0700

    Structure Repo

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|css)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        loader: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
