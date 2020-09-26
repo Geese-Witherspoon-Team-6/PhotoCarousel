442d9c39d3030eaab7bb918fea74b36326e46f5c
ardydj <ardydj07@gmail.com>
2020-09-25 19:50:22 -0700

    Create Test Product Document

--- server/database/index.js
+++ server/database/index.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/qtsy');
+mongoose.connect('mongodb://localhost:27017');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
+debugger;
 db.once('open', function() {
   const productSchema = new mongoose.Schema({
     name: String,
     photos: [String]
   })
+  const Product = mongoose.model('Product', productSchema);
+
+  const blueBird = new Product({
+    name: 'Blue Bird',
+    photos: ['https://images.app.goo.gl/wp1AnspjSPWQiLcYA']});
+  debugger;
 })
 
+
+debugger;
 module.exports = db;
