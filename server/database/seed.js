ad96412c356b8b031641948b9249f0e1445bbeb0
ardydj <ardydj07@gmail.com>
2020-09-29 13:29:51 -0700

    Seeding Script

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,12 +1,25 @@
 const db = require('./index.js');
 const Product = require('./Carousel.js');
+const faker = require('faker');
 
-const blueBird = new Product({
-  name: 'Blue Bird',
-  photos: ['https://images.app.goo.gl/wp1AnspjSPWQiLcYA']});
+// const blueBirdFakeData = new Product({
+//   name: 'Blue Bird',
+//   photos: ['https://images.app.goo.gl/wp1AnspjSPWQiLcYA']});
+const blueBirdFakeData = [];
+
+while (blueBirdFakeData.length !== 100) {
+  let productDoc = {};
+  productDoc.name = faker.commerce.productName();
+  productDoc.photos = [];
+  for (let i = 0; i < 5; i++) {
+    productDoc.photos.push(faker.image.imageUrl());
+  }
+  blueBirdFakeData.push(productDoc);
+}
+// debugger;
 
 const insertFakeData = function () {
-  Product.create(blueBird)
+  Product.create(blueBirdFakeData)
     .then(() => (db.disconnect()));
 };
 
