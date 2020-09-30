6af5b0be50b2402f35d0b0fc2741e9214f6589ea
ardydj <ardydj07@gmail.com>
2020-09-30 12:45:36 -0700

    Add ProductID To Each Document For API Route Params

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,22 +1,24 @@
-const db = require('./index.js');
-const Product = require('./Carousel.js');
+// const db = require('./index.js');
+// const Product = require('./Carousel.js');
 const faker = require('faker');
 
-// const blueBirdFakeData = new Product({
-//   name: 'Blue Bird',
-//   photos: ['https://images.app.goo.gl/wp1AnspjSPWQiLcYA']});
 const blueBirdFakeData = [];
 
+// I am currently using a while loop here and could refactor to a for loop. I had used the while loop before I realized I wanted to manually assign the id for each product instead of using the one created by mongoose.
+let id = 1;
 while (blueBirdFakeData.length !== 100) {
   let productDoc = {};
+  productDoc.productId = id;
   productDoc.name = faker.commerce.productName();
   productDoc.photos = [];
   for (let i = 0; i < 5; i++) {
     productDoc.photos.push(faker.image.imageUrl());
   }
+
   blueBirdFakeData.push(productDoc);
+  id++;
 }
-// debugger;
+
 
 const insertFakeData = function () {
   Product.create(blueBirdFakeData)
