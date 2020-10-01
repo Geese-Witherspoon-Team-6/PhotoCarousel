71e884d1228814c6a1fcc143d3b55aba6bcba91f
ardydj <ardydj07@gmail.com>
2020-10-01 10:25:43 -0700

    Reformat App.js To Match Structure In API Branch

--- server/app.js
+++ server/app.js
@@ -1,4 +1,27 @@
-const mongoose = require('mongoose');
 const db = require('./database/index.js');
-db;
+const express = require('express');
+const bodyParser = require('body-parser');
+const Product = require('./database/Carousel.js');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/../client/dist'))
+
+app.get('/api/carousel/:productId', (req, res) => {
+  Product.find({productId: req.params.productId}, (err, product) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(product);
+    res.send(product)
+  })
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server listening to port ${port}`);
+})
+
+
+module.exports = app;
